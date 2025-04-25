337f4d909be465d36978d88bed06048baddf7325
mm1400 <marin.melentii@gmail.com>
2025-04-25 00:03:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data= {
-      "sheetvision": ["resources/*.png"]
+      "sheetvision": ["resources/**/*.png"]
     },
     install_requires=[
         'numpy',
